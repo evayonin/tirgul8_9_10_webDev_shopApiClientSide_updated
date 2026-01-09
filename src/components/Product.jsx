a06b89baabb232c2a133f982991848bb167dbad1
Eva Yonin <eva.yonin@gmail.com>
2026-01-09 18:32:58 +0200

    הוספת התמונה למוצר שמוצג ללקוח בקטגוריה שנבחרה (תרגול 11)

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,13 @@
 function Product({product ,addToCart}){ // אביה הכניס ערכים מהטבלה עצמה שהיא יצרה אצלה בדאטה בייס
+    // כקגע add to cart לר עושה כלום
     return(
         <div>
           <h2 style={{color:product.color}}>{product.name} - {product.price} $</h2>
           <button onClick={()=>addToCart(product)}>add to cart</button>
+            <img style={{width:"100px" , height:"100px"}} src={product.url} alt={product.name} // יציג את תמונת המוצר (מהרישור בדאטה בייס) , אחרת אם אין יציג שם מוצר
+            />
         </div>
+        // הערה חשובה!!! :
+        // יודע להציג לי את המוצר ככה בגלל setProducts מערך המוצרים בבקשה בקומפוננטה showCategory כשמוכנסים למערך אובייקטים של product לפי המוצרים שחוזרים בבקשה
     )
 }export default Product;
